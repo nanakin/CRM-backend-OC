abaab739fedbd6a8ae58cef4af95a8abefef1e29
Anna Kaczorowski <kaczoro.anna@gmail.com>
2023-11-02 11:21:36 +0100

    add config option for error recording

--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
+"""The main file of the CRM application.
+
+The CRM application is design for employees, customers, contracts and events management using a command-line interface.
+Persistent data are stored in a remote or local database.
+
+This file initialize Model-View-Controller components and execute the user command.
+"""
+
 import sentry_sdk
 import toml
 
@@ -6,7 +14,9 @@ from model import Model
 from view import View
 
 
-def sentry_setup():
+def log_record_setup() -> None:
+    """Initialization of the error monitoring tool."""
+
     sentry_sdk.init(
         dsn="https://2d9b866802f9b43b93fa9a2ea2f3a8a2@o4506144422494208.ingest.sentry.io/4506144429309952",
         # Set traces_sample_rate to 1.0 to capture 100% for performance monitoring.
@@ -16,18 +26,28 @@ def sentry_setup():
     )
 
 
-def main():
-    # keep errors log
-    sentry_setup()
+def main() -> None:
+    """The main entry point of the CRM application."""
 
-    # load config file
+    # load application config file
     config = toml.load("crm.toml")
 
+    # setup for exceptions record
+    do_record = config["error_tracing"]["record"]
+    if do_record:
+        log_record_setup()
+
     # initialize Model-View-Controller components
+
     view = View()
-    model = Model(**config["database"])
-    if config["database"]["reset"] and config["database_sample"]["reset_with_sample"]:
+
+    database_options = config["database"]
+    do_fill_db = config["database"]["reset"] and config["database_sample"]["reset_with_sample"]
+
+    model = Model(**database_options)
+    if do_fill_db:  # testing purpose
         model.populate_with_sample()
+
     controller = Controller(view, model)
 
     # execute user command
--- src/view/cli_input/cli.py
+++ src/view/cli_input/cli.py
@@ -1,30 +1,45 @@
+"""The main file of the command line interface module."""
+
 import sys
 
 import click
 
 from .subcommands import cli_authentication, cli_contract, cli_customer, cli_employee, cli_event
+from view.requests import FullRequest
 
 
 @click.group()
-def cli():
+def cli() -> None:
+    """CRM application allows employees, customers, contracts and events management."""
     pass
 
 
-def cli_main():
+def cli_main() -> FullRequest | None:
+    """Deal with CLI.
+
+    Returns understandable requests to the controller."""
     cli.add_command(cli_employee)
     cli.add_command(cli_customer)
     cli.add_command(cli_authentication)
     cli.add_command(cli_contract)
     cli.add_command(cli_event)
     try:
+        # deal with command line using click module
         returned = cli(standalone_mode=False)
+
+        # if returned == 0:  # when user asks --help
+        #     return None
+        # else:
+        #     return returned
         if type(returned) is tuple:
             request, *param = returned
         else:
-            request, param = returned, ()
+            # if the request has no parameter, give empty list as parameters
+            request, param = returned, list()
+        # transmit user request to the controller
         return request, param
     except click.ClickException as exc:
         exc.show()
         sys.exit(click.ClickException.exit_code)
-    except click.exceptions.Abort:
+    except click.exceptions.Abort:  # ctrl-c
         sys.exit(0)
