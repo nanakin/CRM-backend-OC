4aa3957a5faab434457e2b123ce6bfacf40924bb
Anna Kaczorowski <kaczoro.anna@gmail.com>
2023-11-27 10:16:39 +0100

    add unit tests for model classes (contracts)

---
+++ tests/unit/model/conftest.py
@@ -0,0 +1,37 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from src.model.models import Base
+
+TEST_DB = "sqlite://"  # in-memory SQLite database
+
+
+@pytest.fixture(scope='session')
+def db_engine(request):
+    """yields a SQLAlchemy engine which is suppressed after the test session."""
+    db_url = TEST_DB
+    engine = create_engine(db_url, echo=False)
+    Base.metadata.create_all(engine)
+    yield engine
+
+
+@pytest.fixture(scope='session')
+def db_session_maker(db_engine):
+    """returns a SQLAlchemy scoped session factory."""
+    return sessionmaker(bind=db_engine, expire_on_commit=False)
+
+
+@pytest.fixture(scope='session')
+def db_session_factory(db_session_maker):
+    """returns a SQLAlchemy scoped session factory."""
+    return scoped_session(db_session_maker)
+
+
+@pytest.fixture(scope='function')
+def db_session(db_session_factory):
+    """yields a SQLAlchemy connection which is rollbacked after the test."""
+    session = db_session_factory()
+    yield session
+    session.rollback()
+    session.close()
+
---
+++ tests/unit/model/model_classes/test_model_contract.py
@@ -0,0 +1,71 @@
+import datetime
+import pytest
+from src.model.models import Contract
+from sqlalchemy_utils import force_instant_defaults
+from unittest.mock import Mock
+from uuid import UUID
+
+from sqlalchemy.exc import IntegrityError
+
+force_instant_defaults()
+
+
+def test_contract_empty_constructor(db_session):
+    """Test the contract constructor with no values."""
+    contract = Contract()
+    with pytest.raises(IntegrityError) as e_info:
+        db_session.add(contract)
+        db_session.commit()
+    assert "NOT NULL constraint failed: contract.customer_id" in str(e_info.value)
+
+
+def test_contract_constructor_with_default(db_session):
+    """Test the contract constructor with default values."""
+    contract = Contract(customer_id=3)
+    db_session.add(contract)
+    db_session.commit()
+    assert isinstance(contract.id, UUID)
+    assert isinstance(contract.creation_date, datetime.datetime)
+    assert contract.customer_id == 3
+    assert contract.signed is False
+    assert contract.total_amount == 0
+    assert contract.total_paid == 0
+    assert hasattr(contract, 'customer')
+
+
+def test_get_contract(db_session):
+    """Test the contract constructor with default values."""
+    contract = Contract(customer_id=3)
+    db_session.add_all([contract, Contract(customer_id=4)])
+    db_session.commit()
+    assert Contract.get(db_session, contract.id) == contract
+
+
+def test_contract_update_total_amount():
+    """Test the contract 'update_total_amount' method."""
+    contract = Contract(customer_id=3)
+    contract.update_total_amount(898.99)
+    assert contract.total_amount == 89899
+
+
+def test_contract_add_payment():
+    """Test the contract 'add_payment' method."""
+    contract = Contract(customer_id=3)
+    contract.add_payment(324.3)
+    assert contract.total_paid == 32430
+
+
+def test_as_dict(db_session):
+    """Test the contract 'as_dict' method."""
+    contract = Contract(customer_id=3,
+                        total_amount=89899,
+                        signed=True,
+                        total_paid=3243,
+                        creation_date=datetime.datetime.fromisoformat("2021-12-30-10:02:59"))
+    contract.customer = Mock()
+    contract.customer.commercial_contact = None
+    expected = {'UUID': str(contract.id).upper(), 'Customer': str(contract.customer), 'Commercial': 'None', 'Signed': 'True',
+                'Total due': '866.56 €', 'Total amount': '898.99 €', 'Creation date': '2021-12-30 10:02:59'}
+    result = contract.as_dict()
+    assert result == expected
+
