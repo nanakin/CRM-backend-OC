630bcdd74b5f8fc97ec5df761e3c00ac91eeb502
Anna Kaczorowski <kaczoro.anna@gmail.com>
2023-11-28 10:30:14 +0100

    add a unit test for role-based authentication

---
+++ tests/unit/controller/test_authentication.py
@@ -0,0 +1,39 @@
+from controller import Controller
+from controller.manage.authentication import Auth
+from controller.manage.common import Roles, OperationFailed
+from contextlib import nullcontext as does_not_raise
+import pytest
+from unittest import mock
+
+
+@pytest.fixture
+def initialized_controller():
+    view = mock.Mock()
+    model = mock.Mock()
+    controller = Controller(view, model)
+    return controller
+
+
+def user_with_role(role):
+    return {"username": "test_username", "employee_id": 2, "fullname": "Test Full Name", "role": role}
+
+
+@pytest.mark.parametrize("trying_role, user_profile, expectation, message",
+                         [(Roles.NONE, None, pytest.raises(OperationFailed), "Authentication failed"),
+                          (Roles.NONE, user_with_role(Roles.COMMERCIAL), does_not_raise(), None),
+                          (Roles.ALL, user_with_role(Roles.ADMINISTRATOR), does_not_raise(), None),
+                          (Roles.ALL, user_with_role(Roles.NONE), pytest.raises(OperationFailed), "not have necessary permissions"),
+                          (Roles.ADMINISTRATOR | Roles.SUPPORT, None, pytest.raises(OperationFailed), "Authentication failed"),
+                          (Roles.ADMINISTRATOR | Roles.SUPPORT, user_with_role(Roles.NONE), pytest.raises(OperationFailed), "not have necessary permissions"),
+                          (Roles.ADMINISTRATOR | Roles.SUPPORT, user_with_role(Roles.COMMERCIAL), pytest.raises(OperationFailed), "not have necessary permissions"),
+                          (Roles.ADMINISTRATOR | Roles.SUPPORT, user_with_role(Roles.ADMINISTRATOR), does_not_raise(), None),
+                          (Roles.ADMINISTRATOR | Roles.SUPPORT, user_with_role(Roles.SUPPORT), does_not_raise(), None)])
+def test_controller_authenticate_as_role(initialized_controller, trying_role, user_profile, expectation, message):
+    """Test role-based filter controller method 'authenticate_as_role'."""
+    auth = Auth()
+    if user_profile:
+        auth.identify_as(**user_profile)
+    initialized_controller.authenticate = mock.MagicMock(return_value=auth)
+    with expectation as e:
+        initialized_controller.authenticate_as_role(trying_role)
+    assert message is None or message in str(e)
